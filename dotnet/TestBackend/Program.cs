4ed8ad90db13198078d218e1d64f87d0275a7ee5
Veljko Tosic <veljkotosic6@gmail.com>
2026-01-05 14:56:57 +0100

    Auth rules, controller

--- dotnet/TestBackend/Program.cs
+++ dotnet/TestBackend/Program.cs
@@ -1,11 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TestBackend.Data;
 using TestBackend.Hubs;
+using TestBackend.Models;
 using TestBackend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSignalR();
 builder.Services.AddSingleton<WhiteboardService>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 8;
+
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddCors(options =>
 {
@@ -27,6 +53,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("ReactApp");
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 app.MapHub<WhiteboardHub>("/hubs/whiteboard");
 
 app.Run();
