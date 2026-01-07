f6cc08b1cc9e1ea2f38ce525e4b3abf71d7593ee
StewKI <andrijastev4@gmail.com>
2026-01-07 21:44:57 +0100

    Whiteboard model owner id fix

--- dotnet/AipsCore/Domain/Models/Whiteboard/Whiteboard.cs
+++ dotnet/AipsCore/Domain/Models/Whiteboard/Whiteboard.cs
@@ -6,13 +6,13 @@ namespace AipsCore.Domain.Models.Whiteboard;
 public class Whiteboard
 {
     public WhiteboardId Id { get; private set; }
-    public WhiteboardCode Code { get; private set; }
     public UserId WhiteboardOwnerId { get; private set; }
+    public WhiteboardCode Code { get; private set; }
 
-    public Whiteboard(WhiteboardId id, WhiteboardCode code, UserId whiteboardOwnerId)
+    public Whiteboard(WhiteboardId id, User.User whiteboardOwner, WhiteboardCode code)
     {
         Id = id;
+        WhiteboardOwnerId = whiteboardOwner.Id;
         Code = code;
-        WhiteboardOwnerId = whiteboardOwnerId;
     }
 }
