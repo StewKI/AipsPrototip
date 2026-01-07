e81625fc6e5225d57fff009637176437063b79f6
StewKI <andrijastev4@gmail.com>
2026-01-07 21:58:15 +0100

    username rule add charset

--- dotnet/AipsCore/Domain/Models/User/ValueObjects/Username.cs
+++ dotnet/AipsCore/Domain/Models/User/ValueObjects/Username.cs
@@ -1,6 +1,7 @@
 ﻿using AipsCore.Domain.Common.Validation;
 using AipsCore.Domain.Common.Validation.Rules;
 using AipsCore.Domain.Common.ValueObjects;
+using AipsCore.Domain.Models.User.Validation;
 
 namespace AipsCore.Domain.Models.User.ValueObjects;
 
@@ -14,7 +15,8 @@ public record Username(string UsernameValue) : AbstractValueObject
         return
         [
             new MinLengthRule(UsernameValue, MinimumLength),
-            new MaxLengthRule(UsernameValue, MaximumLength)
+            new MaxLengthRule(UsernameValue, MaximumLength),
+            new UsernameCharsetRule(UsernameValue)
         ];
     }
 }
